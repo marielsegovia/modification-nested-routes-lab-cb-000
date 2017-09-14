27feb8bc37466b7e3299a9493d9cd247dadb3587
Mariel Segovia <msegovia94@gmail.com>
2017-09-14 03:05:26 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show, :new, :edit]
   end
   resources :songs
 end
+
+
+
+#create new song record through an artist -- don't forget to update the strong parameters
